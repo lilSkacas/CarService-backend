65da60b5a5ebca47891d98700d7b9146fe3e315d
Arne <wrong2@gmail.com>
2025-02-24 15:25:27 +0200

    Initial commit

---
+++ src/main/java/lt/ca/javau11/gr/carservice/exception/UserNotFoundException.java
@@ -0,0 +1,11 @@
+package lt.ca.javau11.gr.carservice.exception;
+
+
+public class UserNotFoundException extends RuntimeException {
+
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+
+}
+
